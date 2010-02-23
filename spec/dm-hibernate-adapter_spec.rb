5782a042e802b8fccebac748b8a7b331c95aa561
Piotr Gega (pietia) <piotrgega@gmail.com>
2010-02-23 11:18:15 +0100

    Added configs for 2 more testing dbs

--- spec/dm-hibernate-adapter_spec.rb
+++ spec/dm-hibernate-adapter_spec.rb
@@ -7,8 +7,14 @@ require 'adapter_shared_spec'
 require 'hibernate_shared_spec'
 
 describe DataMapper::Adapters::HibernateAdapter do
+  DB_CONFIGS = {
+          :H2_EMB => { :adapter => "hibernate", :dialect => "H2", :username => "sa", :url => "jdbc:h2:jibernate" },
+          :DERBY_EMB => { :adapter => "hibernate", :dialect => "Derby", :url => "jdbc:derby:jibernate;create=true" },
+          :HSQL_EMB => { :adapter => "hibernate", :dialect => "HSQL", :username => "sa", :url => "jdbc:hsqldb:file:testdb;create=true" }
+  }
+
   before :all do
-    @adapter = DataMapper.setup(:default, :adapter => "hibernate", :dialect => "H2", :username => "sa", :url => "jdbc:h2:jibernate" )
+    @adapter = DataMapper.setup(:default,DB_CONFIGS[:H2_EMB])
   end
 
   it_should_behave_like 'An Adapter'
