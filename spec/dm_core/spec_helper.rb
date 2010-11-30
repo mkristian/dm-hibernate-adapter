e23e8a3dd762d588b8e7f334cbf1a4b0e0aa59c3
douglas <douglasrodrigo@gmail.com>
2010-11-30 19:30:34 -0200

    spec implementation for hibernate adapter without copy dm-core specs

--- spec/dm_core/spec_helper.rb
+++ spec/dm_core/spec_helper.rb
@@ -1,39 +1,5 @@
-require 'pathname'
-require 'rubygems'
-require 'spec'
-require 'dm-hibernate-adapter/spec/setup'
-
-ENV['ADAPTER'] ||= 'in_memory'
-
-SPEC_ROOT = Pathname(__FILE__).dirname.expand_path
-LIB_ROOT = $:.select{|path| path =~ /.+dm-core.+lib/}.first
-
-Pathname.glob((LIB_ROOT  + '/dm-core/spec/**/*.rb'          ).to_s).each { |file| require file }
-Pathname.glob((SPEC_ROOT + '{lib,support,*/shared}/**/*.rb').to_s).each { |file| require file }
-
+require 'dm-core/spec/lib/pending_helpers'
 
 Spec::Runner.configure do |config|
-
-  config.extend( DataMapper::Spec::Adapters::Helpers)
   config.include(DataMapper::Spec::PendingHelpers)
-  config.include(DataMapper::Spec::Helpers)
-
-  config.after :all do
-    DataMapper::Spec.cleanup_models
-  end
-
-  config.after :all do
-    # global ivar cleanup
-    DataMapper::Spec.remove_ivars(self, instance_variables.reject { |ivar| ivar[0, 2] == '@_' })
-  end
-
-  config.after :all do
-    # WTF: rspec holds a reference to the last match for some reason.
-    # When the object ivars are explicitly removed, this causes weird
-    # problems when rspec uses it (!).  Why rspec does this I have no
-    # idea because I cannot determine the intention from the code.
-    DataMapper::Spec.remove_ivars(Spec::Matchers.last_matcher, %w[ @expected ])
-  end
-
 end
-
